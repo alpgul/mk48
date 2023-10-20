c184b01d750f4513bcbc988ee79d3b4d0c8591d3
Alptekin Gülcan <5773704+alpgul@users.noreply.github.com>
2023-10-20 16:17:08 +0300

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,7 +12,7 @@ apt-get install -y \
   openssl
 
 ## Install rustup and common components
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
 rustup override set nightly-2022-08-14
 rustup target add wasm32-unknown-unknown
 cargo install --version 0.15.0  --locked trunk
